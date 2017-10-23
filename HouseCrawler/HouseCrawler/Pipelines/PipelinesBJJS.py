71acdae9378dc419e8210868f822c216e437475b
王朝丰 <chaofeng.wang@yunfangdata.com>
2017-10-23 14:34:20 +0800

    optimize settings

--- HouseCrawler/HouseCrawler/Pipelines/PipelinesBJJS.py
+++ HouseCrawler/HouseCrawler/Pipelines/PipelinesBJJS.py
@@ -3,7 +3,7 @@ import sys
 import os
 import logging
 import uuid
-from HouseCrawler.Items.ItemsBJJS import *
+import datetime
 sys.path.append(os.path.abspath('.'))
 sys.path.append(os.path.abspath('..'))
 sys.path.append(os.path.abspath('../..'))
@@ -20,21 +20,9 @@ class BJJSPipeline(object):
     def from_crawler(cls, crawler):
         return cls(crawler.settings)
 
-    def check_exist(self, item):
-        q_object = item.django_model.objects
-        if isinstance(item, ProjectBaseItem):
-            pass
-        elif isinstance(item, ProjectInfoItem):
-            pass
-        elif isinstance(item, BuildingInfoItem):
-            pass
-        elif isinstance(item, HouseInfoItem):
-            pass
-
     def process_item(self, item, spider):
         if hasattr(item, 'save') and hasattr(item, 'django_model'):
             item['RecordID'] = uuid.uuid1()
-            
-
+            item['CurTimeStamp'] = str(datetime.datetime.now())
             item.save()
         return item
