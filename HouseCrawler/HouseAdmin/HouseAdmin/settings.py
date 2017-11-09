6457b67ca5dc24918f681ce954bbb28659151207
王朝丰 <chaofeng.wang@yunfangdata.com>
2017-11-09 16:41:11 +0800

    finish ver1 cq develop

--- HouseCrawler/HouseAdmin/HouseAdmin/settings.py
+++ HouseCrawler/HouseAdmin/HouseAdmin/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
 
 MONGODB_DATABASES = {
     "default": {
-        "name": 'HouseNew_Dalian',
+        "name": 'HouseNew_Chongqing',
         "host": '192.168.6.204',
         "tz_aware": True,
     },
--- HouseCrawler/HouseAdmin/HouseNew/models.py
+++ HouseCrawler/HouseAdmin/HouseNew/models.py
@@ -15,14 +15,12 @@ class ProjectBase(Document):
     ProjectDistrict = fields.StringField(default='', max_length=1024, null=False)
     ProjectAddress = fields.StringField(default='', max_length=1024, null=False)
     ProjectCorporation = fields.StringField(default='', max_length=1024, null=False)
-    ProjectURL = fields.URLField(default=None, null=True, blank=True)
-    ProjectURLCurTimeStamp = fields.StringField(default=str(datetime.datetime.now()), index=True)
     meta = {
         'indexes': [
             'CurTimeStamp',
             'ProjectUUID',
             'ProjectName',
-            'ProjectURL',
+            'ProjectCorporation'
         ]
     }
 
@@ -35,16 +33,15 @@ class ProjectInfo(Document):
                                     binary=False, null=False)
     ProjectName = fields.StringField(default='', max_length=1024, null=False)
     ProjectAddress = fields.StringField(default='', max_length=1024, null=False)
-    ProjectCompany = fields.StringField(default='', max_length=1024, null=False)
-    ProjectRegName = fields.StringField(default='', max_length=1024, null=False)
-    ProjectAreaPlanLicenseCode = fields.StringField(default='', max_length=1024, null=False)
-    ProjectPlanLicenseCode = fields.StringField(default='', max_length=1024, null=False)
-    ProjectSaleSum = fields.DictField(default={'code': 0}, null=False)
+    ProjectCorporation = fields.StringField(default='', max_length=1024, null=False)
+    ProjectSoldNum = fields.StringField(default='0', max_length=1024, null=False)
+    ProjectArea = fields.StringField(default='0.0', max_length=1024, null=False)
     meta = {
         'indexes': [
             'CurTimeStamp',
             'ProjectUUID',
             'ProjectName',
+            'ProjectCorporation'
         ]
     }
 
@@ -53,14 +50,18 @@ class BuildingInfo(Document):
     RecordID = fields.UUIDField(default=uuid.uuid1(),
                                     binary=True, primary_key=True, null=False)
     CurTimeStamp = fields.StringField(default=str(datetime.datetime.now()), index=True)
-    ProjectName = fields.StringField(default='', max_length=1024, null=False)
     ProjectUUID = fields.UUIDField(default=uuid.uuid3(uuid.NAMESPACE_DNS, ''),
                                     binary=False, null=False)
+    ProjectName = fields.StringField(default='', max_length=1024, null=False)
+    SubProjectUUID = fields.UUIDField(default=uuid.uuid3(uuid.NAMESPACE_DNS, ''),
+                                    binary=False, null=False)
+    SubProjectName = fields.StringField(default='', max_length=1024, null=False)
+    ProjectAddress = fields.StringField(default='', max_length=1024, null=False)
+    ProjectCorporation = fields.StringField(default='', max_length=1024, null=False)
     BuildingRegName = fields.StringField(default='', max_length=1024, null=False)
     BuildingUUID = fields.UUIDField(default=uuid.uuid3(uuid.NAMESPACE_DNS, ''),
                                     binary=False, null=False)
     BuildingName = fields.StringField(default='', max_length=1024, null=False)
-    BuildingAddress = fields.StringField(default='', max_length=1024, null=False)
     BuildingHouseNum = fields.StringField(default='', max_length=1024, null=False)
     BuildingArea = fields.StringField(default='', max_length=1024, null=False)
     BuildingURL = fields.URLField(default=None, null=True, blank=True)
@@ -70,6 +71,8 @@ class BuildingInfo(Document):
             'CurTimeStamp',
             'ProjectName',
             'ProjectUUID',
+            'SubProjectUUID',
+            'SubProjectName',
             'BuildingRegName',
             'BuildingUUID',
             'BuildingName',
@@ -90,10 +93,105 @@ class HouseInfo(Document):
     HouseUUID = fields.UUIDField(default=uuid.uuid3(uuid.NAMESPACE_DNS, ''),
                                     binary=False, null=False)
     HouseName = fields.StringField(default='', max_length=1024, null=False)
-    HouseFloor = fields.StringField(default='', max_length=1024, null=False)
     HouseSaleState = fields.StringField(default='', max_length=1024, null=False)
     HouseSaleStateLatest = fields.StringField(default='', max_length=1024, null=False)
     HouseInfoStr = fields.StringField(default='', max_length=1024, null=False)
+
+
+    HoueseID = fields.StringField(default='', max_length=1024, null=False)
+    HoueseRegID = fields.StringField(default='', max_length=1024, null=False)
+    HouseFloor = fields.StringField(default='', max_length=1024, null=False)
+    HouseRoomNum = fields.StringField(default='', max_length=1024, null=False)
+    "y":13,
+    "x":2,
+    "bArea":116.3,
+    "iArea":85.74,
+    "vArea":0,
+    "sArea":30.56,
+    "shareRate":0,
+    "uArea":0,
+    "prebArea":0,
+    "preiArea":0,
+    "prevArea":0,
+    "presArea":0,
+    "preShareRate":0,
+    "preuArea":0,
+    "stru":"钢筋混凝土结构",
+    "struId":0,
+    "rType":"二室二厅",
+    "rTypeId":0,
+    "landOwnType":0,
+    "landKind":0,
+    "landParcelMemoId":0,
+    "useId":0,
+    "landUserId":0,
+    "use":"成套住宅",
+    "noSale":false,
+    "memo":null,
+    "u":0,
+    "d":0,
+    "l":0,
+    "r":0,
+    "status":2621452,
+    "stasn":null,
+    "stan":null,
+    "pid":0,
+    "ex1":null,
+    "tag":"11064585",
+    "unitnumber":"1",
+    "block":"1",
+    "roomIid":null,
+    "location":"渝中区嘉滨路3号3-2",
+    "siteId":0,
+    "storeyHeight":0,
+    "floorUse":0,
+    "verandas":0,
+    "areaType":0,
+    "preSalearea":0,
+    "saleArea":0,
+    "parcelUsearea":0,
+    "parecelSharearea":0,
+    "state":0,
+    "publicField":null,
+    "parcelStartDate":null,
+    "parcelEndDate":null,
+    "isUnion":0,
+    "nsjg":45600,
+    "nsjmjg":33600,
+    "sellerId":0,
+    "enterpriseId":0,
+    "district":0,
+    "rightKindId":0,
+    "rightTypeId":0,
+    "f_zxsp_name":null,
+    "zxsp":0,
+    "yslx":null,
+    "isYjs":0,
+    "ysxgsj":null,
+    "beizu":null,
+    "fsbw":null,
+    "F_ISONLINESIGN":1,
+    "F_ISCONTRACT":1,
+    "F_ISOTHERRIGHT":0,
+    "F_ISOWNERSHIP":0,
+    "F_ISLIMIT":0,
+    "F_ISUSE":0,
+    "S_ISONLINESIGN":null,
+    "S_ISCONTRACT":null,
+    "S_ISOTHERRIGHT":null,
+    "S_ISOWNERSHIP":null,
+    "S_ISLIMIT":null,
+    "S_ISUSE":null,
+    "F_PRESALE_CERT":null,
+    "F_HOUSE_NO":"YZ00600600790000010100100130002",
+    "F_FJH":null,
+    "F_STATE":"期房",
+    "F_Business_Status":null,
+    "f_nosale_memo":"",
+    "F_Total":0
+
+
+
     meta = {
         'indexes': [
             'CurTimeStamp',
