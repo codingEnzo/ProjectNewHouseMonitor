d0b27a78bf12870938c7e1464f96c1b22fb9ad2a
王朝丰 <chaofeng.wang@yunfangdata.com>
2017-11-09 22:06:12 +0800

    finish dev model

--- HouseCrawler/HouseAdmin/HouseNew/models.py
+++ HouseCrawler/HouseAdmin/HouseNew/models.py
@@ -93,103 +93,22 @@ class HouseInfo(Document):
     HouseUUID = fields.UUIDField(default=uuid.uuid3(uuid.NAMESPACE_DNS, ''),
                                     binary=False, null=False)
     HouseName = fields.StringField(default='', max_length=1024, null=False)
-    HouseSaleState = fields.StringField(default='', max_length=1024, null=False)
-    HouseSaleStateLatest = fields.StringField(default='', max_length=1024, null=False)
-    HouseInfoStr = fields.StringField(default='', max_length=1024, null=False)
-
-
     HoueseID = fields.StringField(default='', max_length=1024, null=False)
     HoueseRegID = fields.StringField(default='', max_length=1024, null=False)
     HouseFloor = fields.StringField(default='', max_length=1024, null=False)
     HouseRoomNum = fields.StringField(default='', max_length=1024, null=False)
     HouseBuildingArea = fields.StringField(default='', max_length=1024, null=False)
     HouseInnerArea = fields.StringField(default='', max_length=1024, null=False)
-    "vArea":0,
-    "sArea":30.56,
-    "shareRate":0,
-    "uArea":0,
-    "prebArea":0,
-    "preiArea":0,
-    "prevArea":0,
-    "presArea":0,
-    "preShareRate":0,
-    "preuArea":0,
-    "stru":"钢筋混凝土结构",
-    "struId":0,
-    "rType":"二室二厅",
-    "rTypeId":0,
-    "landOwnType":0,
-    "landKind":0,
-    "landParcelMemoId":0,
-    "useId":0,
-    "landUserId":0,
-    "use":"成套住宅",
-    "noSale":false,
-    "memo":null,
-    "u":0,
-    "d":0,
-    "l":0,
-    "r":0,
-    "status":2621452,
-    "stasn":null,
-    "stan":null,
-    "pid":0,
-    "ex1":null,
-    "tag":"11064585",
-    "unitnumber":"1",
-    "block":"1",
-    "roomIid":null,
-    "location":"渝中区嘉滨路3号3-2",
-    "siteId":0,
-    "storeyHeight":0,
-    "floorUse":0,
-    "verandas":0,
-    "areaType":0,
-    "preSalearea":0,
-    "saleArea":0,
-    "parcelUsearea":0,
-    "parecelSharearea":0,
-    "state":0,
-    "publicField":null,
-    "parcelStartDate":null,
-    "parcelEndDate":null,
-    "isUnion":0,
-    "nsjg":45600,
-    "nsjmjg":33600,
-    "sellerId":0,
-    "enterpriseId":0,
-    "district":0,
-    "rightKindId":0,
-    "rightTypeId":0,
-    "f_zxsp_name":null,
-    "zxsp":0,
-    "yslx":null,
-    "isYjs":0,
-    "ysxgsj":null,
-    "beizu":null,
-    "fsbw":null,
-    "F_ISONLINESIGN":1,
-    "F_ISCONTRACT":1,
-    "F_ISOTHERRIGHT":0,
-    "F_ISOWNERSHIP":0,
-    "F_ISLIMIT":0,
-    "F_ISUSE":0,
-    "S_ISONLINESIGN":null,
-    "S_ISCONTRACT":null,
-    "S_ISOTHERRIGHT":null,
-    "S_ISOWNERSHIP":null,
-    "S_ISLIMIT":null,
-    "S_ISUSE":null,
-    "F_PRESALE_CERT":null,
-    "F_HOUSE_NO":"YZ00600600790000010100100130002",
-    "F_FJH":null,
-    "F_STATE":"期房",
-    "F_Business_Status":null,
-    "f_nosale_memo":"",
-    "F_Total":0
-
-
-
+    HouseVArea = fields.StringField(default='', max_length=1024, null=False)
+    HouseShareArea = fields.StringField(default='', max_length=1024, null=False)
+    HouseStructure = fields.StringField(default='', max_length=1024, null=False)
+    HouseRoomType = fields.StringField(default='', max_length=1024, null=False)
+    HouseUsage = fields.StringField(default='', max_length=1024, null=False)
+    HouseSaleState = fields.StringField(default='', max_length=1024, null=False)
+    HouseSaleStateLatest = fields.StringField(default='', max_length=1024, null=False)
+    HouseInnerUnitPrice = fields.StringField(default='', max_length=1024, null=False)
+    HouseBuildingUnitPrice = fields.StringField(default='', max_length=1024, null=False)
+    HouseType = fields.StringField(default='', max_length=1024, null=False)
     meta = {
         'indexes': [
             'CurTimeStamp',
