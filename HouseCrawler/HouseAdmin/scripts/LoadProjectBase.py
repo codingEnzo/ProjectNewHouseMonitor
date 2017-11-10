bc3cb144e07cd932fab207f23ec7e4af83f830ba
王朝丰 <chaofeng.wang@yunfangdata.com>
2017-11-10 18:19:58 +0800

    finish dev ver1

--- HouseCrawler/HouseAdmin/scripts/LoadProjectBase.py
+++ HouseCrawler/HouseAdmin/scripts/LoadProjectBase.py
@@ -5,7 +5,7 @@ import redis
 
 def run():
     r = redis.Redis('10.30.1.20')
-    project_base = {'source_url': 'http://www.dlfd.gov.cn/fdc/D01XmxxAction.do?Control=select',
+    project_base = {'source_url': 'http://www.cq315house.com/315web/HtmlPage/SpfQuery.htm',
                     'meta': {'PageType': 'ProjectBase'}}
     project_base_json = json.dumps(project_base, sort_keys=True)
     r.sadd('HouseCrawler:start_urls:Default:Chongqing', project_base_json)
