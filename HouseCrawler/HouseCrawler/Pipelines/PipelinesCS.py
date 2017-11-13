f6e2302ce500f37da62cd88f19fc4e035fc95a62
王朝丰 <chaofeng.wang@yunfangdata.com>
2017-11-13 20:40:54 +0800

    finish dev ver1

--- HouseCrawler/HouseCrawler/Pipelines/PipelinesCS.py
+++ HouseCrawler/HouseCrawler/Pipelines/PipelinesCS.py
@@ -12,7 +12,7 @@ sys.path.append(os.path.abspath('../..'))
 logger = logging.getLogger(__name__)
 
 
-class CSpipeline(object):
+class CSPipeline(object):
 
     def __init__(self, settings):
         self.settings = settings
