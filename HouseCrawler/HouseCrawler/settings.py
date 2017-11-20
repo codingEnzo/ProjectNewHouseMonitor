d33a391c37f0df78d71dacdf033569468eecf758
王朝丰 <chaofeng.wang@yunfangdata.com>
2017-11-20 16:20:21 +0800

    finish dev ver1

--- HouseCrawler/HouseCrawler/settings.py
+++ HouseCrawler/HouseCrawler/settings.py
@@ -39,7 +39,7 @@ COMMANDS_MODULE = 'HouseCrawler.Commands'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 40
 
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
