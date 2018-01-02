2b2f0f97b63b4d6708068810d95514353acee3dc
jacksonSun <jiajia.sun@yunfangdata.com>
2018-01-02 16:00:40 +0800

    add fuzhou

---
+++ HouseCrawler/AirflowAdmin/ServiceCore/HouseCrawler/Items/ItemsFuzhou.py
@@ -0,0 +1,19 @@
+# coding = utf-8
+from scrapy_djangoitem import DjangoItem
+
+from HouseNew.models import *
+
+class SearchProjectBaseItem(DjangoItem):
+    django_model = SearchProjectBaseFuzhou
+
+class ProjectinfoBaseItem(DjangoItem):
+    django_model = ProjectinfoBaseFuzhou
+
+class ApprovalBaseItem(DjangoItem):
+    django_model = ApprovalBaseFuzhou
+
+class BuildingBaseItem(DjangoItem):
+    django_model = BuildingBaseFuzhou
+
+class HouseBaseItem(DjangoItem):
+    django_model = HouseBaseFuzhous
