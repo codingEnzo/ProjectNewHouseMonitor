ead68485aba50b76d44aae9262c4af9b13af9cc7
chiufung <chiufung@localhost.localdomain>
2018-01-24 10:42:40 +0800

    东莞、济南、太原、合肥、河源 BuildingInfo层redis缓存机制key ttl漏洞修复

--- HouseCrawler/AirflowAdmin/dags/NewHouseJN.py
+++ HouseCrawler/AirflowAdmin/dags/NewHouseJN.py
@@ -145,7 +145,7 @@ def cacheLoader(key=REDIS_CACHE_KEY):
         except Exception:
             import traceback
             traceback.print_exc()
-        r.expire(key, spider_settings.get('CLOSESPIDER_TIMEOUT'))
+        r.expire(key, int(spider_settings.get('CLOSESPIDER_TIMEOUT')))
 
 
 t3 = PythonOperator(
