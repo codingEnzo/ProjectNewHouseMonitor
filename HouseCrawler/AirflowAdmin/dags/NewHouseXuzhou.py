88e59b08da28eef2339e1cd3fdc50b5253fa8322
chiufung <chiufung@localhost.localdomain>
2018-03-08 14:30:08 +0800

    迁移徐州到airflow,并对接kafka

--- HouseCrawler/AirflowAdmin/dags/NewHouseXuzhou.py
+++ HouseCrawler/AirflowAdmin/dags/NewHouseXuzhou.py
@@ -54,8 +54,8 @@ default_args = {
 spider_settings = {
     'ITEM_PIPELINES': {
         'HouseCrawler.Pipelines.PipelinesXuzhou.PipelineXuzhou': 300,
-        'HouseCrawler.Pipelines.PipelinesUtils.PipelinesCheck.CheckPipeline': 299,
-        'HouseCrawler.Pipelines.PipelinesUtils.PipelinesKafka.KafkaPipeline': 301,
+        # 'HouseCrawler.Pipelines.PipelinesUtils.PipelinesCheck.CheckPipeline': 299,
+        # 'HouseCrawler.Pipelines.PipelinesUtils.PipelinesKafka.KafkaPipeline': 301,
     },
     'SPIDER_MIDDLEWARES': {
         'HouseCrawler.SpiderMiddleWares.SpiderMiddleWaresXuzhou.ProjectBaseHandleMiddleware': 103,
