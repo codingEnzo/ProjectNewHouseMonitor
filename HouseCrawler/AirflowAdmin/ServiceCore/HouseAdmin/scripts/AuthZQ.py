73df2dd5e73439bcf5da34c64301529ab056c9b3
1012137875@qq.com <chiufung@localhost.localdomain>
2018-06-19 09:36:59 +0800

    肇庆增加sessionid获取中间件

--- HouseCrawler/AirflowAdmin/ServiceCore/HouseAdmin/scripts/AuthZQ.py
+++ HouseCrawler/AirflowAdmin/ServiceCore/HouseAdmin/scripts/AuthZQ.py
@@ -8,11 +8,15 @@ def run():
     while True:
         proxies = r.smembers('GZYF_Test:Proxy_Pool:H')
         for proxy in proxies:
-            proxy_tmp = "http://%s" % proxy.decode()
-            authURL = 'http://61.146.213.163:8011/user_kfs.aspx'
-            res = req.get(authURL, proxies={
-                'http': proxy_tmp, 'https': proxy_tmp}, timeout=5)
-            r.set(proxy_tmp, 'ASP.NET_SessionId: %s' %
-                  res.cookies.get('ASP.NET_SessionId', ''))
-            r.expire(proxy_tmp, 30)
-            print(proxy_tmp)
+            try:
+                proxy_tmp = "http://%s" % proxy.decode()
+                authURL = 'http://61.146.213.163:8011/user_kfs.aspx'
+                res = req.get(authURL, proxies={
+                    'http': proxy_tmp, 'https': proxy_tmp}, timeout=5)
+                r.set(proxy_tmp, 'ASP.NET_SessionId: %s' %
+                      res.cookies.get('ASP.NET_SessionId', ''))
+                r.expire(proxy_tmp, 30)
+                print(proxy_tmp)
+            except Exception:
+                import traceback
+                traceback.print_exc()
